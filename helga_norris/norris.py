2a04540fe02b0268320bf3adbf7d52cc4c93f1a4
Shaun Duncan <shaun.duncan@gmail.com>
2013-12-10 16:28:17 -0500

    Update plugin for helga v1.0 api

--- helga_norris/norris.py
+++ helga_norris/norris.py
@@ -1,54 +1,21 @@
-import random as _random
-
 import pkg_resources
+import random
 
-from helga.extensions.base import (CommandExtension,
-                                   ContextualExtension)
-from helga.log import setup_logger
-
-
-logger = setup_logger(__name__)
+from helga.plugins import command
 
 
 _quips = [quip.rstrip().decode('utf-8') for quip in
-    pkg_resources.resource_stream('helga_norris', 'quips.txt')
-    if quip.rstrip()]
+          pkg_resources.resource_stream('helga_norris', 'quips.txt')
+          if quip.rstrip()]
 
 
-class NorrisExtension(CommandExtension):
+@command('norris', help="Chuck Norris facts for specific users. Usage: helga norris <nick>")
+def norris(client, channel, nick, message, cmd, args):
+    global _quips
 
-    NAME = 'norris'
-
-    usage = 'norris <username>'
-
-    allow_many = True
-
-    def __init__(self, *args, **kwargs):
-        self._quips = _quips
-        super(NorrisExtension, self).__init__(*args, **kwargs)
-
-    def handle_message(self, opts, message):
-        username = opts['<username>']
-        message.response = self.norris(username=username)
-
-    def should_handle_message(self, opts, message):
-        if not opts:
-            return False
-
-        if opts.get('norris'):
-            if opts.get('<username>'):
-                return True
-
-        logger.info('returning false :( :(')
-        return False
-
-
-    def norris(self, username=None):
-        """
-        Hook into giphypop to find a gif. If no search term, just
-        return a random gif. If a search term is given, try to translate
-        it and default back to a search
-        """
-        sentence = _random.choice(_quips)
-        sentence = sentence.replace('Chuck Norris', username)
-        return sentence
+    try:
+        username = args[0]
+    except IndexError:
+        return '{0}, I need a nick to use'.format(nick)
+    else:
+        return random.choice(_quips).replace('Chuck Norris', username)
